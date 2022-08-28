f74a02bcde931540815ea84d86dff8923a53dc32
Admin Admin <admin@drtywrk.com>
2022-08-28 14:22:44 -0700

    Update about page

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -41,7 +41,7 @@ const AboutPage = () => {
 
           <div className={css.contentWrapper}>
             <div className={css.contentSide}>
-              <p>"We want to change people's minds about resale activewear. If you recycle plastic bottles and containers, you should also be recycling your plastic clothing aka leggings."</p>
+              <p>"We want to change people's minds about resale activewear. If you recycle plastic bottles and containers, you should also be recycling your plastic clothing aka polyblend fabrics."</p>
             </div>
 
             <div className={css.contentMain}>
@@ -53,12 +53,12 @@ const AboutPage = () => {
               When you sell your unwanted activewear you give them a second life. Not only are you doing something responsible for the planet but you'll find some amazing deals and discounts on brands you love like Alo Yoga, Lululemon and Nike. 
               </p>
 
-              <p>With DRTYWRK, we want to make sure you're transaction will go smoothly: from browsing and checking the stock, to making the order and payment, to the review of the sellers. And we hope you'll be so convinced that you'll soon start selling your no longer used items to make new buyers happy!</p>
+              <p>With DRTYWRK, we want to make sure you're transaction will go smoothly. And we hope you'll be so convinced that you'll soon start selling your no longer used activewear to make new buyers and the earth happy!</p>
 
               <h3 className={css.subtitle}>Do you have activewear to sell?</h3>
 
               <p>
-                DRTYWRK offers you a good way to earn some extra cash! If you're not using your
+                DRTYWRK offers you an easy way to earn some extra cash! If you're not using your
                 activewear anymore, why not sell it? And if you already have laid your eyes on something you want next, selling something from your collection is a great way to get money for your next buy and make room in your closet!
               </p>
 
@@ -66,16 +66,16 @@ const AboutPage = () => {
               </p>
 
               <p>Fees: 
-                <li>Sell yourself and keep 80% of the sale. 
+                <li>Sell yourself and keep up to 80% of the sale. 
                 </li>
 
-                <li>Have a large number of items to sell and don't have the time? Contact us <a href="mailto:hello@drtywrk.com">here</a> and we'll wash, list and ship for a fee.</li>
+                <li>Have a large number of items to sell or don't have time to list them yourself? Contact us <a href="mailto:hello@drtywrk.com">here</a> and we'll wash, list and ship for a fee.</li>
               </p>
 
               <p>
                 You can also checkout our{' '}
                 <ExternalLink href={siteFacebookPage}>Facebook</ExternalLink> and{' '}
-                <ExternalLink href={siteInstagramPage}>Instagram</ExternalLink>.
+                <ExternalLink href={siteInstagramPage}>Instagram</ExternalLink>. Message us to be featured wearing your favorite activewear outfits.
               </p>
             </div>
           </div>
